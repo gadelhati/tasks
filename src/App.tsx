a18c770c1bdac81027fa4439e7e016a816f2ee05
gadelhati <gadelha.ti@gmail.com>
2026-03-24 16:44:00 -0300

    feat: add kanban

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import { Tasks } from './container/tasks'
-import { TasksEisenhower } from './container/tasksEisenhower'
+// import { Tasks } from './container/tasks'
+// import { TasksEisenhower } from './container/tasksEisenhower'
+import { TasksKanban } from './container/tasksKanban'
 
 function App() {
   return (
     <>
       {/* <Tasks /> */}
-      <TasksEisenhower />
+      {/* <TasksEisenhower /> */}
+      <TasksKanban />
     </>
   )
 }
